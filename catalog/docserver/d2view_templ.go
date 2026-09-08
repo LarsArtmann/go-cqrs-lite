8aa26324cd30d38605093d551dabcfd3a92eddc0
Lars Artmann <git@lars.software>
2026-09-08 22:26:41 +0200

    chore: auto-commit 4 changed file(s) (heuristic)

--- catalog/docserver/d2view_templ.go
+++ catalog/docserver/d2view_templ.go
@@ -99,7 +99,7 @@ func D2Page(brand, docsPrefix, diagram string) templ.Component {
 				var templ_7745c5c3_Var4 string
 				templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(diagram)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `catalog/docserver/d2view.templ`, Line: 38, Col: 20}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `d2view.templ`, Line: 38, Col: 20}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 				if templ_7745c5c3_Err != nil {
